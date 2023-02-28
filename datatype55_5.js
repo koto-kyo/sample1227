04d435169187bffa7480e8abc78eeb3fd03ea04a
maeda <test@test.com>
2023-02-28 10:29:42 +0000

    add 55_5 file

---
+++ datatype55_5.js
@@ -0,0 +1,11 @@
+["Bilbo","Gandalf","Nazgu1"].forEach((item,index,array) => {
+    console.log(`${item} is at index ${index} in ${array}`)
+});
+
+(item) => {
+    console.log(item)
+}
+
+function arry2(item){
+    console.log(item)
+}
