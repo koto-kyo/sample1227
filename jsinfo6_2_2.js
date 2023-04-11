3c758ca0a4d82b896f3212297cc1d4bf22a6d07e
maeda <test@test.com>
2023-04-11 10:16:38 +0000

    add jsinfo6_3_1

---
+++ jsinfo6_2_2.js
@@ -0,0 +1,4 @@
+let arr1 = [1, -2, 3, 4];
+let arr2 = [8, 3, -8, 1];
+
+console.log( Math.max(...arr1, ...arr2));
---
+++ jsinfo6_3_1.js
@@ -0,0 +1,8 @@
+let message = "Hi";
+{
+    let message = "Hello";
+
+    console.log(message);
+}
+
+console.log(message);
