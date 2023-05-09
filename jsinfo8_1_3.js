8f4a3d117045926f948d8add8b5d589583817329
maeda <test@test.com>
2023-05-09 10:59:32 +0000

    add jsinfo8_1_3

---
+++ jsinfo8_1_3.js
@@ -0,0 +1,20 @@
+let animal = {
+    walk() {
+        if(!this.isSleeping) {
+            console.log(`I walk`) ;
+        }
+    },
+    sleep() {
+        this.isSleeping = true;
+    }
+};
+
+let rabbit = {
+    name: "White Rabbit",
+    __proto__: animal
+};
+
+rabbit.sleep();
+
+console.log(rabbit.isSleeping);
+console.log(animal.isSleeping);
