ad78ee143903c4e4c14c8da7b182c13f9e2b21cb
maeda <test@test.com>
2023-07-11 11:02:06 +0000

    add server.cj

---
+++ jsinfo10_1.1.js
@@ -0,0 +1,9 @@
+try {
+    console.log('Start of try runs');
+
+    lalala;
+
+    console.log('End of try never reached');
+} catch(err) {
+    console.log('Error has occured');
+}
--- jsinfo11_2_2.js
+++
@@ -1,9 +0,0 @@
-new Promise(function(resolve , reject){
-    setTimeout(() => resolve(1),1000);
-}).then(function(result) {
-    console.log(result);
-    return result * 2;
- }).then(function(result) {
-        console.log(result);
-        return result *2;
-    }).then( { function(result))
